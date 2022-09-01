aa97a5c840eff73d0ef0a3bdadef33bf2451ef0b
Marcos Villabasa <marcosvillabasa@gmail.com>
2022-09-01 15:23:59 -0300

    feat(AddItem): created custom hook for the add item issue

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -2,38 +2,38 @@ import React from 'react'
 import { Button, StyleSheet, TextInput, View } from 'react-native'
 
 const AddItem = ({ onChange, onPress, isDisabled, value }) => {
-    return (
-        <View style={styles.itemContainer}>
-            <TextInput
-                value={value}
-                placeholder='Add Item'
-                style={styles.input}
-                selectionColor='#016fb9'
-                onChangeText={(e) => onChange(e)}
-            />
-            <Button
-                title='Add'
-                onPress={onPress}
-                color='#016fb9'
-                disabled={isDisabled}
-            />
-        </View>
-    )
+	return (
+		<View style={styles.itemContainer}>
+			<TextInput
+				value={value}
+				placeholder='Add Item'
+				style={styles.input}
+				selectionColor='#016fb9'
+				onChangeText={(e) => onChange(e)}
+			/>
+			<Button
+				title='Add'
+				onPress={onPress}
+				color='#016fb9'
+				disabled={isDisabled}
+			/>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-    input: {
-        borderBottomColor: '#016fb9',
-        borderBottomWidth: 1,
-        height: 45,
-        width: '75%'
-    },
-    itemContainer: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        marginVertical: 10
-    }
-});
+	input: {
+		borderBottomColor: '#016fb9',
+		borderBottomWidth: 1,
+		height: 45,
+		width: '75%',
+	},
+	itemContainer: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		marginVertical: 10,
+	},
+})
 
 export default AddItem
