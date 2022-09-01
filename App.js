294514aad6f33c5952e4744bf08a2e61e6cb3062
Marcos Villabasa <marcosvillabasa@gmail.com>
2022-09-01 16:29:51 -0300

    feat(Home): created Home screen

--- App.js
+++ App.js
@@ -2,26 +2,15 @@ import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 import AddItem from './src/components/AddItem'
 import useListItems from './src/hooks/useListItems'
+import Home from './src/screens/Home'
 
 export default function App() {
 	const { addItem, handleChange, value, items } = useListItems()
 
 	return (
-		<View style={styles.container}>
-			<AddItem
-				onPress={addItem}
-				onChange={handleChange}
-				isDisabled={value === ''}
-				value={value}
-			/>
-			<View style={styles.itemsContainer}>
-				{items.map((item) => (
-					<View key={item.id}>
-						<Text>{item.name}</Text>
-					</View>
-				))}
+			<View style={styles.container}>
+				<Home />
 			</View>
-		</View>
 	)
 }
 
@@ -29,10 +18,7 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		color: 'white',
-		margin: 50,
 		marginHorizontal: 10,
+    marginTop: 50
 	},
-  itemsContainer: {
-    marginHorizontal: 10
-  }
 })
