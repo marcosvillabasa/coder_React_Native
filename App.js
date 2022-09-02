6214cdc80708ab86359fddd894bba9d234dfef0e
Marcos Villabasa <marcosvillabasa@gmail.com>
2022-09-02 17:20:38 -0300

    refactor(App): add safe area view in app

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native'
 import Home from './src/screens/Home'
 
 export default function App() {
 	return (
-		<View style={styles.container}>
+		<SafeAreaView>
 			<Home />
-		</View>
+		</SafeAreaView>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		color: 'white',
-		marginHorizontal: 10,
-		marginTop: 50,
-	},
-})
