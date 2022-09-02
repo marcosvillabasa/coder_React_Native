db04eaf98c7b81a1b5cd94ae5ece55b64346d8a4
Marcos Villabasa <marcosvillabasa@gmail.com>
2022-09-02 17:13:56 -0300

    feat(Item): added styles in Item component

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 const Item = ({ name }) => (
-	<View>
-		<Text>{name}</Text>
+	<View style={styles.itemContainer}>
+		<Text style={styles.itemText} >{name}</Text>
 	</View>
 )
 
+const styles = StyleSheet.create({
+	itemContainer: {
+		padding: 10,
+		marginVertical: 10,
+        backgroundColor: '#0193f4'
+	},
+    itemText: {
+        color: '#ffffff',
+        fontSize: 18,
+        fontWeight: '600',
+        textAlign: 'center'
+        
+    }
+})
 export default Item
